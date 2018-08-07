3c24677a02fac91f74507373fb8db9eb4e6a0943
ddlcmoddev <Justin+ddlcmoddev@users.noreply.github.com>
2018-08-07 02:11:36 +0200

    Updated location API with secure acces key

--- scripts/geo.py
+++ scripts/geo.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 send_url = "http://api.ipstack.com/check?access_key=d12f72d834c71d8c72ca5face5f22bac&security=1"
+#secure key
 geo_req = requests.get(send_url)
 geo_json = json.loads(geo_req.text)
 
@@ -13,5 +14,6 @@ country = geo_json['country_name']
 
 
 print("I've always wanted to go to " + city + ", " + country)
+
 
 
